8e85c221ab392de442a2019f337e9236c699e153
lmxy1990 <lmxy1990@foxmail.com>
2018-11-09 18:00:09 +0800

    update

--- src/main/java/github/slimrpc/core/constant/ClientDaemonThreadEventType.java
+++ src/main/java/github/slimrpc/core/constant/ClientDaemonThreadEventType.java
@@ -2,12 +2,6 @@ package github.slimrpc.core.constant;
 
 public class ClientDaemonThreadEventType {
 	public static final byte noEvent = 0;
-
-	@Deprecated
-	public static final byte channelConnecting = 1;
-
-	@Deprecated
-	public static final byte channelConnected = 2;
 	public static final byte channelDisconnected = 3;
 	public static final byte closeDaemonThread = 4;
 }
