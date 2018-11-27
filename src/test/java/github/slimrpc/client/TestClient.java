b2d0e162e9e942a819cb04d047ca4e1b9bd263b7
lmxy1990 <lmxy1990@foxmail.com>
2018-11-27 12:36:18 +0800

    add test

--- src/test/java/github/slimrpc/client/TestClient.java
+++ src/test/java/github/slimrpc/client/TestClient.java
@@ -1,45 +1,45 @@
-//package github.slimrpc.client;
+package github.slimrpc.client;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * TestClient
+ * <p>
+ * 释义:
+ *
+ * @author: xinyi.pan
+ * @create: 2018-11-06 09:42
+ **/
+
+@ContextConfiguration({"/spring-context-client.xml"})
+public class TestClient extends AbstractJUnit4SpringContextTests {
+
+/*    @Autowired
+    @Qualifier("userService")
+    private UserService userService ;*/
+
+
+    @Test
+    public void testSayHello(){
+
+//        String mayun = userService.sayHello("mayun");
+
+        System.out.println("");
+
+    }
+
+
+    @Test
+    public void testMyClassParam() {
+//        UserOption option  = new UserOption("zhangsan","32") ;
 //
-//import org.junit.Test;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-//
-///**
-// * TestClient
-// * <p>
-// * 释义:
-// *
-// * @author: xinyi.pan
-// * @create: 2018-11-06 09:42
-// **/
-//
-//@ContextConfiguration({"/spring-context-client.xml"})
-//public class TestClient extends AbstractJUnit4SpringContextTests {
-//
-///*    @Autowired
-//    @Qualifier("userService")
-//    private UserService userService ;*/
-//
-//
-//    @Test
-//    public void testSayHello(){
-//
-////        String mayun = userService.sayHello("mayun");
-//
-//        System.out.println("");
-//
-//    }
-//
-//
-//    @Test
-//    public void testMyClassParam() {
-////        UserOption option  = new UserOption("zhangsan","32") ;
-////
-////        UserResult login = userService.login(option);
-//
-//        System.out.println("");
-//
-//    }
-//
-//
-//}
+//        UserResult login = userService.login(option);
+
+        System.out.println("");
+
+    }
+
+
+}
--- src/test/java/github/slimrpc/service/UserService.java
+++ src/test/java/github/slimrpc/service/UserService.java
@@ -1,21 +1,21 @@
-//package github.slimrpc.service;
-//
-//import github.slimrpc.domain.UserOption;
-//import github.slimrpc.domain.UserResult;
-//
-///**
-// * UserService
-// * <p>
-// * 释义:
-// *
-// * @author: xinyi.pan
-// * @create: 2018-11-06 09:39
-// **/
-//public interface UserService {
-//
-//    String sayHello(String name) ;
-//
-//
-//    UserResult login(UserOption userOption) ;
-//
-//}
+package github.slimrpc.service;
+
+import github.slimrpc.domain.UserOption;
+import github.slimrpc.domain.UserResult;
+
+/**
+ * UserService
+ * <p>
+ * 释义:
+ *
+ * @author: xinyi.pan
+ * @create: 2018-11-06 09:39
+ **/
+public interface UserService {
+
+    String sayHello(String name) ;
+
+
+    UserResult login(UserOption userOption) ;
+
+}
